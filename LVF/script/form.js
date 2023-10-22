77cda20004899e6d1030c8634d3ad70e0bf9c9f8
Somenae <kevindevaux@neuf.fr>
2023-10-22 13:56:44 +0200

    formulaire créé

--- LVF/script/form.js
+++ LVF/script/form.js
@@ -6,14 +6,18 @@ export default class contactForm extends ManageDom {
         this.render();
     }
     render() {
-        let footer = document.getElementsByTagName("footer");
+        const footer = this.createMarkup("footer", "", document.body);
         const form = this.createMarkup("form", "", footer, [{"class":"formulaire"}]);
-        const inputName = this.createMarkup("input", "Nom", form, [{"type":"text"}, {"placeholder":"Nom"}, {"class":"input"}, {"id":"name"}, {"name":"name"}]);
-        const inputFirstName = this.createMarkup("input", "Prénom", form, [{"type":"text"}, {"placeholder":"Prénom"}, {"class":"input"}, {"id":"firstName"}, {"name":"firstName"}]);
-        const inputMail = this.createMarkup("input", "Email", form, [{"type":"text"}, {"placeholder":"Mail"}, {"class":"input"}, {"id":"email"}, {"name":"email"}]);
-        const result = this.createMarkup("div", "", form);
-        return{
-            form
-        }
+        const labelName = this.createMarkup("label", "Nom", form, [{"class":"label"}, {"for":"name"}]);
+        const inputName = this.createMarkup("input", "", form, [{"type":"text"}, {"placeholder":"Nom"}, {"class":"input"}, {"id":"name"}, {"name":"name"}]);
+        const labelFirstName = this.createMarkup("label", "Prénom", form, [{"class":"label"}, {"for":"firstName"}]);
+        const inputFirstName = this.createMarkup("input", "", form, [{"type":"text"}, {"placeholder":"Prénom"}, {"class":"input"}, {"id":"firstName"}, {"name":"firstName"}]);
+        const labelMail = this.createMarkup("label", "Email", form, [{"class":"label"}, {"for":"email"}]);
+        const inputMail = this.createMarkup("input", "", form, [{"type":"text"}, {"placeholder":"Mail"}, {"class":"input"}, {"id":"email"}, {"name":"email"}]);
+        const labelPhone = this.createMarkup("label", "Téléphone", form, [{"class":"label"}, {"for":"phone"}]);
+        const inputPhone = this.createMarkup("input", "", form, [{"type":"text"}, {"placeholder":"Téléphone"}, {"class":"input"}, {"id":"phone"}, {"name":"phone"}]);
+        const labelHow = this.createMarkup("label", "Comment avez-vous entendu parler de nous ?", form, [{"class":"label"}, {"for":"how"}]);
+        const inputHow = this.createMarkup("input", "", form, [{"type":"text"}, {"placeholder":"Comment avez-vous entendu parler de nous ?"}, {"class":"input"}, {"id":"how"}, {"name":"how"}]);
+        const inputMessage = this.createMarkup("textarea", "", form, [{"type":"text"}, {"placeholder":"Message"}, {"class":"textarea"}, {"id":"message"}, {"name":"message"}]);
     }
 }
