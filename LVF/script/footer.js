271c9405e0c50903658f253ecb50bbcc469efa3b
Tontico <antho.noex@live.fr>
2023-10-23 15:51:54 +0200

    modif footer.js

--- LVF/script/footer.js
+++ LVF/script/footer.js
@@ -8,10 +8,12 @@ export default class footer extends ManageDom {
 
     render(){
         const footer = this.createMarkup("footer", "", document.body);
-        const divLogo = this.createMarkup("div", "", footer);
+        const divContainer = this.createMarkup("div", "", footer, [{"class":"div-container"}])
+        const divLogo = this.createMarkup("div", "", divContainer, [{"class":"div-logo"}]);
         const logo = this.createMarkup("img", "", divLogo, [{src: "./assets/imgs/lvf-logo+txt-alpha.png"}]);
-        const nav = this.createMarkup("nav", "", footer);
+        const nav = this.createMarkup("nav", "", divContainer, [{"class":"ul-box"}]);
         const ul = this.createMarkup("ul", "", nav);
+        let divSocialTab = this.createMarkup("div", "",divContainer, [{"class":"div-social-tab"}]);
         let i = 0;
         let liTab = [];
         let aTab1 = [];
@@ -31,10 +33,9 @@ export default class footer extends ManageDom {
         aTab1[2].innerText = "Notre atelier";
         aTab1[3].innerText = "Nos Vélomobiles";
         aTab1[4].innerText = "Contactez-nous";
-        const p = this.createMarkup("p", "Suivez-nous !", nav);
         i = 0;
         while(i<4){
-            aTab2[i] = this.createMarkup("a", "", nav);
+            aTab2[i] = this.createMarkup("a", "", divSocialTab);
             socialTab[i] = this.createMarkup("img", "", aTab2[i]);
             i++;
         }
@@ -66,32 +67,4 @@ export default class footer extends ManageDom {
             console.log("Pouet2");
         }
     }
-    i = 0;
-    while (i < 5) {
-      aTab[i] = this.createMarkup("a", "", liTab[i]);
-      i++;
-    }
-    aTab[0].innerText = "Accueil";
-    aTab[1].innerText = "Notre histoire";
-    aTab[2].innerText = "Notre atelier";
-    aTab[3].innerText = "Nos Vélomobiles";
-    aTab[4].innerText = "Contactez-nous";
-    aTab[0].href = "./index.html";
-    aTab[1].href = "./html/histoire.html";
-    aTab[2].href = "./html/usines.html";
-    aTab[3].href = "./html/modeles.html";
-    aTab[4].href = "#";
-    
-    i = 0;
-    while (i < 4) {
-      aTab[i] = this.createMarkup("a", "", nav);
-
-      socialTab[i] = this.createMarkup("img", "", divSocialTab);
-      i++;
-    }
-    socialTab[0].src = "./assets/imgs/facebook-f.svg";
-    socialTab[1].src = "./assets/imgs/instagram.svg";
-    socialTab[2].src = "./assets/imgs/youtube.svg";
-    socialTab[3].src = "./assets/imgs/dailymotion.svg";
-  }
 }
